824baa893c8a51a2912cec2107101e588307946d
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-02 12:20:49 -0300

    feat: criar CustomerRepository

---
+++ src/main/java/com/itau/transferencia/repositories/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.itau.transferencia.repositories;
+
+import com.itau.transferencia.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByAccount(String account);
+}
