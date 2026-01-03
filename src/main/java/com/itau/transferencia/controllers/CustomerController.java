e3eab13addefc911be34ecc04d659ac08dd186e5
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-02 21:52:13 -0300

    feat: add endpoints and use service

--- src/main/java/com/itau/transferencia/controllers/CustomerController.java
+++ src/main/java/com/itau/transferencia/controllers/CustomerController.java
@@ -1,46 +1,61 @@
 package com.itau.transferencia.controllers;
 
 import com.itau.transferencia.entities.Customer;
-import com.itau.transferencia.http.HttpHelper;
-import com.itau.transferencia.repositories.CustomerRepository;
-import com.itau.transferencia.http.CustomerRequest;
+import com.itau.transferencia.entities.Transfer;
+import com.itau.transferencia.http.helpers.HttpHelper;
+import com.itau.transferencia.http.requests.CustomerRequest;
+import com.itau.transferencia.http.requests.TransferRequest;
+import com.itau.transferencia.http.responses.TransferResponse;
+import com.itau.transferencia.services.CustomerService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/v1/customers")
 public class CustomerController {
+    private final CustomerService service;
 
-    private final CustomerRepository repository;
-
-    public CustomerController(CustomerRepository customerRepository) {
-        this.repository = customerRepository;
+    public CustomerController(CustomerService service) {
+        this.service = service;
     }
 
     @GetMapping
     public ResponseEntity<List<Customer>> getAll() {
-        var customers = repository.findAll();
+        var customers = service.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(customers);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<Customer> findByAccount(@RequestParam String account) {
-        return repository.findByAccount(account)
+    public ResponseEntity<Customer> findByAccountNumber(@RequestParam String account) {
+        return service.findByAccount(account)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<Customer> create(@Valid @RequestBody CustomerRequest request) {
-        var newCustomer = repository.save(Customer.fromRequest(request));
-        var location = HttpHelper.getLocation(newCustomer);
+        var newCustomer = service.create(request);
+        var location = HttpHelper.getLocation(newCustomer.getId());
         return ResponseEntity.created(location).body(newCustomer);
+    }
+
+    @GetMapping("/{account}/transfers")
+    public ResponseEntity<?> getTransfers(@PathVariable String account) {
+        var transfers = service.getTransfers(account);
+        return ResponseEntity.ok(transfers);
+    }
+
+    @PostMapping("/{account}/transfers")
+    public ResponseEntity<TransferResponse> transfer(@PathVariable String account, @Valid @RequestBody TransferRequest transferRequest) {
+        var transfer = service.transfer(account, transferRequest);
+        var response = TransferResponse.fromEntity(transfer);
+        return ResponseEntity.status(CREATED).body(response);
     }
 }
