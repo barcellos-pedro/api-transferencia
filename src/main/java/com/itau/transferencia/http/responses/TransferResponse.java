ca05503b0338dd32f15154222767359faf1a7239
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-03 10:13:07 -0300

    refactor: handle nullable Customer and format date

--- src/main/java/com/itau/transferencia/http/responses/TransferResponse.java
+++ src/main/java/com/itau/transferencia/http/responses/TransferResponse.java
@@ -1,23 +1,36 @@
 package com.itau.transferencia.http.responses;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.itau.transferencia.entities.Customer;
 import com.itau.transferencia.entities.Transfer;
+import com.itau.transferencia.entities.TransferStatus;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public record TransferResponse(
         Long id,
         String sourceAccount,
         String destinationAccount,
         BigDecimal amount,
-        LocalDateTime createdAt
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+        LocalDateTime createdAt,
+        TransferStatus status
 ) {
     public static TransferResponse fromEntity(Transfer transfer) {
-        var source = transfer.getSource().getAccount();
-        var destination = transfer.getDestination().getAccount();
-        var id = transfer.getId();
+        var destination = getAccount(transfer.getDestination());
+        var source = getAccount(transfer.getSource());
+        var createdAt = transfer.getCreatedAt();
         var amount = transfer.getAmount();
-        var dateTime = transfer.getCreatedAt();
-        return new TransferResponse(id, source, destination, amount, dateTime);
+        var status = transfer.getStatus();
+        var id = transfer.getId();
+        return new TransferResponse(id, source, destination, amount, createdAt, status);
+    }
+
+    private static String getAccount(Customer customer) {
+        return Optional.ofNullable(customer)
+                .map(Customer::getAccount)
+                .orElse("INVALID_ACCOUNT");
     }
 }
