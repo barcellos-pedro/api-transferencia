939d28184333edee3b31753718545630667681e7
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-03 10:10:18 -0300

    feat: use TransferService

--- src/main/java/com/itau/transferencia/controllers/CustomerController.java
+++ src/main/java/com/itau/transferencia/controllers/CustomerController.java
@@ -1,18 +1,17 @@
 package com.itau.transferencia.controllers;
 
 import com.itau.transferencia.entities.Customer;
-import com.itau.transferencia.entities.Transfer;
 import com.itau.transferencia.http.helpers.HttpHelper;
 import com.itau.transferencia.http.requests.CustomerRequest;
 import com.itau.transferencia.http.requests.TransferRequest;
 import com.itau.transferencia.http.responses.TransferResponse;
 import com.itau.transferencia.services.CustomerService;
+import com.itau.transferencia.services.TransferService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
@@ -20,41 +19,46 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RestController
 @RequestMapping("/v1/customers")
 public class CustomerController {
-    private final CustomerService service;
+    private final CustomerService customerService;
+    private final TransferService transferService;
 
-    public CustomerController(CustomerService service) {
-        this.service = service;
+    public CustomerController(CustomerService customerService, TransferService transferService) {
+        this.customerService = customerService;
+        this.transferService = transferService;
     }
 
     @GetMapping
     public ResponseEntity<List<Customer>> getAll() {
-        var customers = service.findAll();
+        var customers = customerService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(customers);
     }
 
     @GetMapping("/search")
     public ResponseEntity<Customer> findByAccountNumber(@RequestParam String account) {
-        return service.findByAccount(account)
+        return customerService.findByAccount(account)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<Customer> create(@Valid @RequestBody CustomerRequest request) {
-        var newCustomer = service.create(request);
+        var newCustomer = customerService.create(request);
         var location = HttpHelper.getLocation(newCustomer.getId());
         return ResponseEntity.created(location).body(newCustomer);
     }
 
     @GetMapping("/{account}/transfers")
     public ResponseEntity<?> getTransfers(@PathVariable String account) {
-        var transfers = service.getTransfers(account);
+        var transfers = transferService.getTransfers(account);
         return ResponseEntity.ok(transfers);
     }
 
     @PostMapping("/{account}/transfers")
-    public ResponseEntity<TransferResponse> transfer(@PathVariable String account, @Valid @RequestBody TransferRequest transferRequest) {
-        var transfer = service.transfer(account, transferRequest);
+    public ResponseEntity<TransferResponse> transfer(
+            @PathVariable String account,
+            @Valid @RequestBody TransferRequest transferRequest
+    ) {
+        var transfer = transferService.transfer(account, transferRequest);
         var response = TransferResponse.fromEntity(transfer);
         return ResponseEntity.status(CREATED).body(response);
     }
