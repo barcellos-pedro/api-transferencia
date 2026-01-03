a6123051e1fe36a4c85f6bac81ca9944713d095a
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-02 21:50:34 -0300

    feat: add ErrorResponse helper

---
+++ src/main/java/com/itau/transferencia/http/helpers/ErrorResponse.java
@@ -0,0 +1,30 @@
+package com.itau.transferencia.http.helpers;
+
+import com.itau.transferencia.http.responses.Errors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+
+public class ErrorResponse {
+    private ErrorResponse() {
+    }
+
+    public static ResponseEntity<Errors> badRequest(RuntimeException exception) {
+        return ResponseEntity.badRequest().body(Errors.of(exception));
+    }
+
+    public static ResponseEntity<Errors> badRequest(String message) {
+        return ResponseEntity.badRequest().body(Errors.of(message));
+    }
+
+    public static ResponseEntity<Errors> badRequest(List<String> message) {
+        return ResponseEntity.badRequest().body(Errors.of(message));
+    }
+
+    public static ResponseEntity<Errors> conflict(String message) {
+        return ResponseEntity.status(CONFLICT).body(Errors.of(message));
+    }
+}
