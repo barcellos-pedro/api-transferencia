fab1a021b350a97e0d2df4f602384f58bec64451
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-02 21:52:28 -0300

    chore: remove public modifier

--- src/main/java/com/itau/transferencia/TransferenciaApplication.java
+++ src/main/java/com/itau/transferencia/TransferenciaApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransferenciaApplication {
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(TransferenciaApplication.class, args);
     }
 }
