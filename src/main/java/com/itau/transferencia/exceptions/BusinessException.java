8b4cfe7f271d22a808b1cb1fde0a84903dcf2d80
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-04 14:03:35 -0300

    refactor: remove redundant initializer

--- src/main/java/com/itau/transferencia/exceptions/BusinessException.java
+++ src/main/java/com/itau/transferencia/exceptions/BusinessException.java
@@ -3,7 +3,7 @@ package com.itau.transferencia.exceptions;
 import org.springframework.http.HttpStatus;
 
 public class BusinessException extends RuntimeException {
-    private HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    private HttpStatus httpStatus;
 
     public BusinessException(String message, HttpStatus httpStatus) {
         super(message);
