31c7a9893bc65e3c21f2975109fa5dd27ccd0f57
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-04 11:49:34 -0300

    refactor: update validation messages

--- src/main/java/com/itau/transferencia/http/requests/CustomerRequest.java
+++ src/main/java/com/itau/transferencia/http/requests/CustomerRequest.java
@@ -1,20 +1,19 @@
 package com.itau.transferencia.http.requests;
 
 import com.itau.transferencia.validations.AccountNumber;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 import java.math.BigDecimal;
 
 public record CustomerRequest(
-        @NotBlank(message = "Name is required")
+        @NotBlank(message = "is required")
         String name,
 
         @AccountNumber
         String account,
 
+        @Min(value = 0, message = "must be positive")
         BigDecimal balance
 ) {
-    public CustomerRequest {
-        balance = BigDecimal.ZERO;
-    }
 }
