f3095d8a800b3ec32263df58059aa69bde331035
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-04 14:01:14 -0300

    refactor: delete helper class

--- src/main/java/com/itau/transferencia/advices/ControllerAdvice.java
+++ src/main/java/com/itau/transferencia/advices/ControllerAdvice.java
@@ -1,7 +1,6 @@
 package com.itau.transferencia.advices;
 
 import com.itau.transferencia.exceptions.BusinessException;
-import com.itau.transferencia.http.helpers.ErrorResponse;
 import com.itau.transferencia.http.responses.Errors;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
@@ -13,33 +12,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static com.itau.transferencia.advices.ControllerAdviceHelper.getErrorsMessage;
 import static com.itau.transferencia.exceptions.ErrorMessages.*;
+import static org.springframework.http.HttpStatus.CONFLICT;
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<Errors> handleBusinessException(BusinessException exception) {
-        return ErrorResponse.fromBusinessException(exception);
+        return ResponseEntity.status(exception.getHttpStatus())
+                .body(Errors.of(exception));
     }
 
     @ExceptionHandler(ObjectOptimisticLockingFailureException.class)
     public ResponseEntity<Errors> handleOptimisticLockingException(ObjectOptimisticLockingFailureException exception) {
-        return ErrorResponse.badRequest(OPTIMISTIC_LOCK);
+        return ResponseEntity.badRequest().body(Errors.of(OPTIMISTIC_LOCK));
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Errors> handleDataIntegrityException(DataIntegrityViolationException exception) {
-        return ErrorResponse.conflict(ACCOUNT_EXISTS);
+        return ResponseEntity.status(CONFLICT).body(Errors.of(ACCOUNT_EXISTS));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Errors> handleValidationException(MethodArgumentNotValidException exception) {
         var errors = getErrorsMessage(exception);
-        return ErrorResponse.badRequest(errors);
+        return ResponseEntity.badRequest().body(Errors.of(errors));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Errors> handleRequestBodyException(HttpMessageNotReadableException exception) {
-        return ErrorResponse.badRequest(BAD_JSON);
+        return ResponseEntity.badRequest().body(Errors.of(BAD_JSON));
     }
 }
