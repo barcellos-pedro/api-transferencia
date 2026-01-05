9d74d20e7ccdc2f753834a6829a4dabda0501379
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-04 22:36:38 -0300

    chore: delete unused exception class

--- src/main/java/com/itau/transferencia/exceptions/UniqueAccountException.java
+++
@@ -1,9 +0,0 @@
-package com.itau.transferencia.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class UniqueAccountException extends BusinessException {
-    public UniqueAccountException(String message, HttpStatus httpStatus) {
-        super(message, httpStatus);
-    }
-}
