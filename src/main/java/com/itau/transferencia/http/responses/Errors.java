3cd292333008261faab46edd06f60f2e6c57e171
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-01-04 21:58:46 -0300

    refactor: delete method

--- src/main/java/com/itau/transferencia/http/responses/Errors.java
+++ src/main/java/com/itau/transferencia/http/responses/Errors.java
@@ -10,8 +10,4 @@ public record Errors(List<String> errors) {
     public static Errors of(List<String> errors) {
         return new Errors(errors);
     }
-
-    public static Errors of(RuntimeException ex) {
-        return new Errors(List.of(ex.getMessage()));
-    }
 }
